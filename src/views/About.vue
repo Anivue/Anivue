772a2a159ebe555c882b94920df1c871928c59d3
VadimZb <vvz.dev@gmail.com>
2021-01-28 19:17:29 +0300

    media cards created

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <media-grid />
 </template>
+
+<script>
+import MediaGrid from "../components/MediaGrid";
+export default {
+    name: "About",
+    components: {
+        MediaGrid,
+    },
+};
+</script>
