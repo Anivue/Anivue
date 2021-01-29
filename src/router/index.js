44c3275e0a2c1e4820225bccbe35c96cfa1212c7
VadimZb <vvz.dev@gmail.com>
2021-01-29 23:42:33 +0300

    vue router scrolling behavior set to scroll to top on every route

--- src/router/index.js
+++ src/router/index.js
@@ -36,6 +36,9 @@ const router = new VueRouter({
     mode: "history",
     base: process.env.BASE_URL,
     routes,
+    scrollBehavior() {
+        return { x: 0, y: 0 };
+    },
 });
 
 export default router;
