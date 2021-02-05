b892841c5e41da77cfd2f762f176a02fefb0e9b1
VadimZb <vvz.dev@gmail.com>
2021-02-06 00:26:10 +0300

    deploy script added; minor fixes

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+cd dist
+cp index.html 404.html
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:VadimZb/anibase.git master:gh-pages
+cd -
