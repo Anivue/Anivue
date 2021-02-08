c31886272f73bc04d06a4387208a2d612174ff4d
VadimZb <vvz.dev@gmail.com>
2021-02-08 19:59:01 +0300

    pwa config changed

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     transpileDependencies: ["vuetify"],
     publicPath: "/",
+    pwa: {
+        name: "Anivue",
+        themeColor: "#272727",
+        msTileColor: "#272727",
+        manifestOptions: {
+            background_color: "#272727",
+        },
+    },
 };
