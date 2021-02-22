bebc6272e6c9f41fd832bd81b9e62365b770431c
VadimZb <vvz.dev@gmail.com>
2021-02-22 14:39:56 +0300

    pwa manifest start url fixed

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 module.exports = {
     transpileDependencies: ["vuetify"],
-    publicPath: "/",
     pwa: {
         name: "Anivue",
         themeColor: "#272727",
         msTileColor: "#272727",
+        appleMobileWebAppCapable: "yes",
         manifestOptions: {
             background_color: "#272727",
+            start_url: "/home",
         },
     },
 };
